c6df89c6759fb3c76941815979ddcaac3605552a
nguyen tri đạt <nguyentridat@MacBook-Air-cua-nguyen.local>
2023-11-16 19:03:03 +0700

    thi

--- thi.php/thiphp/app/Http/Controllers/productcontroller.php
+++ thi.php/thiphp/app/Http/Controllers/productcontroller.php
@@ -2,9 +2,27 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\product;
 use Illuminate\Http\Request;
 
 class productcontroller extends Controller
 {
-    //
+    public function create(){
+        return view("products.add");
+    }
+    public function add(Request $request){
+        $rule=[];
+        $message=[];
+        $this->validate($request,$rule,$message);
+
+        $data=$request->all();
+        $data["ProductType"]=$request->request("ProductType");
+        $data["ProductCode"]=$request->request("ProductCode");
+        $data["ProductName"]=$request->request("ProductName");
+        $data["Quantity"]=$request->input("Quantity");
+        $data["Note"]=$request->input("Note");
+
+        product::create($data);
+        return redirect()->route("product","index");
+    }
 }
